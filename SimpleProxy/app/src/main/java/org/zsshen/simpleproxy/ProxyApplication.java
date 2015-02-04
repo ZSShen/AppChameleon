61472eed56afa3dfa7ca1204ebf2f0b2755a7a80
ZSShen <andy780328@hotmail.com>
2015-02-04 23:58:56 +0800

    Create the simple proxy application to start the PoC of Android packer.

---
+++ SimpleProxy/app/src/main/java/org/zsshen/simpleproxy/ProxyApplication.java
@@ -0,0 +1,16 @@
+package org.zsshen.simpleproxy;
+
+import android.app.Application;
+import android.content.Context;
+
+public class ProxyApplication extends Application {
+    protected void attachBaseContext(Context base)
+    {
+        return;
+    }
+
+    public void onCreate()
+    {
+
+    }
+}
