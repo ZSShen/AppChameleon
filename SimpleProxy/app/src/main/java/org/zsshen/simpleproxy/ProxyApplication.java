277fe6fbea6c3edd2d8d6b03a68a438d02d3b6bc
ZSShen <andy780328@hotmail.com>
2015-02-05 02:02:40 +0800

    Create the simple proxy application to start the PoC of Android packer.

--- SimpleProxy/app/src/main/java/org/zsshen/simpleproxy/ProxyApplication.java
+++ SimpleProxy/app/src/main/java/org/zsshen/simpleproxy/ProxyApplication.java
@@ -2,15 +2,54 @@ package org.zsshen.simpleproxy;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ProxyApplication extends Application {
-    protected void attachBaseContext(Context base)
+    static private String NAME_ORIGINAL_APK = "Original.apk";
+
+    protected void attachBaseContext(Context ctxBase)
     {
+        boolean bRtnCode = true;
+
+        bRtnCode = getOriginalDex(ctxBase);
+        if (!bRtnCode)
+            return;
+
         return;
     }
 
     public void onCreate()
     {
+        return;
+    }
 
+    private boolean getOriginalDex(Context ctxBase)
+    {
+        boolean bRtnCode = true;
+
+        AssetManager astMgr = ctxBase.getAssets();
+        try {
+            InputStream isAst = astMgr.open(NAME_ORIGINAL_APK);
+            ZipInputStream zisApk = new ZipInputStream(new BufferedInputStream(isAst));
+            while (true) {
+                ZipEntry entry = zisApk.getNextEntry();
+                if (entry == null)
+                    break;
+                String szEntry = entry.getName();
+                Log.d("Proxy Unpack", szEntry);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            bRtnCode = false;
+        }
+        return bRtnCode;
     }
 }
